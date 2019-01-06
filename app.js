de8144645af7a08d0c791fe0748a29928cc8f71f
Mike Ajala <agmt5989@gmail.com>
2019-01-06 01:48:12 +0100

    /search endpoint

--- app.js
+++ app.js
@@ -10,7 +10,23 @@ var bucket = cluster.openBucket("test");
 app.use(Cors());
  
 app.get("/search", (request, response) => {
-	
+    var SearchQuery = Couchbase.SearchQuery;
+    var query = SearchQuery.new("idx-music", SearchQuery.match(request.query.query).fuzziness(1));
+    query.fields(["title", "artist"]);
+    bucket.query(query, function(error, result, meta) {
+        if(error) {
+            return response.status(500).send({ message: error.message });
+        }
+        result = result.map(item => {
+            return {
+                id: item.id,
+                score: item.score,
+                title: item.fields.title,
+                artist: item.fields.artist
+            };
+        });
+        response.send(result);
+    });
 });
  
 app.listen(3000, () => {
